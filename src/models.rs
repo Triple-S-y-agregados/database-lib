d792a4b0a04ab2f81f556b1f9bee11dc6fa6f65a
Salva HG <salva.hg01@gmail.com>
2021-05-14 03:58:02 -0500

    Add insertable new record

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,15 @@
-#[derive(Queryable)]
+use super::schema::records;
+
+#[derive(Queryable,Clone)]
 pub struct Record {
     pub id: i32,
     pub timestamp: String,
     pub voltage: i32,
+}
+
+#[derive(Insertable)]
+#[table_name="records"]
+pub struct NewRecord<'a> {
+    pub timestamp: &'a str,
+    pub voltage: &'a i32,
 }
