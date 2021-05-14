947814aca8db7b679004d008030b2e14e6c4f84e
Salva HG <salva.hg01@gmail.com>
2021-05-14 03:21:10 -0500

    Add function to show records and test

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,25 @@ pub fn establish_connection() -> SqliteConnection {
         .expect(&format!("Error connecting to {}", database_url))
 }
 
+pub fn show_records() {
+    use schema::records::dsl::*;
+
+    let connection = establish_connection();
+
+    let results = records
+        .load::<models::Record>(&connection)
+        .expect("Error loading records");
+
+        println!("Displaying {} records", results.len());
+        for post in results {
+            println!("----------\n");
+            println!("{}", post.id);
+            println!("{}", post.timestamp);
+            println!("{}", post.voltage);
+            println!("----------\n");
+        }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -26,5 +45,10 @@ mod tests {
     #[test]
     fn database_connection_test() {
         establish_connection();
+    }
+
+    #[test]
+    fn show_records_test() {
+        show_records();
     }
 }
